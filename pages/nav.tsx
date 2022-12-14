ff3b7361ce15d8bf136ced2e2c5bf8a56e132bae
ensarince <32306029+ensarince@users.noreply.github.com>
2022-12-14 10:51:48 +0100

    navbar v11

--- pages/nav.tsx
+++ pages/nav.tsx
@@ -22,9 +22,9 @@ function Nav({}: Props) {
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
-              <a href="/">
+              <Link href="/">
                 <h2 className="text-2xl sm:text-xl text-white font-bold">MyClimbs</h2>
-              </a>
+              </Link>
               <div className="md:hidden">
                 <button
                   className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
